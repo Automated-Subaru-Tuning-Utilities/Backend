f6248b109289d086728f1ee0c0f494e501fd0ca9
W-Dominic <dom0_o@protonmail.com>
2022-08-12 15:52:29 -0400

    restructured files

---
+++ lowmaf/models/lowmaf.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class lowmaf_data(BaseModel):
+    time: int 
+    af_correction_short: float
+    af_correction_learning: float
+    intake_air_temp: int
+    mass_airflow_voltage: float
+    cl_ol_status: int
+
--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 from typing import List
 
-class lowmaf(BaseModel):
-    time: int 
-    af_correction_short: float
-    af_correction_learning: float
-    intake_air_temp: int
-    mass_airflow_voltage: float
-    cl_ol_status: int
+import sys
+sys.path.append("lowmaf/models/")
+from lowmaf import lowmaf_data 
 
 app = FastAPI()
 
-@app.post("/api/{parameters}")
-async def read_data( parameters:str, log: List[lowmaf] ):
-    if (parameters == "lowmaf"):
-        return log
-    return {
-        "parameters": parameters
-    }
+#lowmaf
+@app.post("/api/analyze/0/")
+async def read_data( log: List[lowmaf_data] ):
+    return log
